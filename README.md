b8dcc6c725d40a0ba152955bd755b6138a43ed3f
ama002 <aashish.mahaswary@cambiumnetworks.com>
2017-07-20 12:34:08 +0530

    Failing Test

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 This repository contains the following tests.
 
-# 1-csv_convert
--------------
+# failing_tests
+
+The file "test_code.py" contains details of the test. It contains 3 unit tests
+that all fail. Modify the code where indicated to fix the failing tests.
+
+# csv_convert
 
 Read the file "csv_question.txt" for more information. Create a script to
 convert a file to a CSV file. Example input files are included in the folder.
 
-# 2-unit_convert
---------------
+# unit_convert
 
 The file "unit_convert.py" contains details of the test. All of the doc tests
 for the existing function fail. Write the code, using the doc string as a 
---
+++ failing_tests/test_code.py
@@ -0,0 +1,112 @@
+# You should be able to use the python interpreter to run this script,
+# and it should display 3 test failures. You need to fix the tests so
+# that they all pass.
+#
+# IMPORTANT:
+# Only modify the code where indicated. Do not modify the intent of the test
+#
+import unittest
+
+class PythonTests(unittest.TestCase):
+    def test_multipliers(self):
+        # Fix this loop and function definition so that the test
+        # passes. 'multipliers' should become a list of functions. The
+        # first should be a function that multiplies its parameter by
+        # 0, the second by 1, the third by 2, and so on.
+        
+        # MODIFY IN HERE ONLY
+        
+        multipliers = []
+        for val in range(6):
+            multiplier = lambda x: x * val
+            multipliers.append(multiplier)
+
+        # END MODIFY IN HERE ONLY
+
+        # Call each function with the parameter 5. We expect to get
+        # the 5 times table.
+        expected_result = [0, 5, 10, 15, 20, 25]
+        actual_result = []
+        for multiplier in multipliers:
+            actual_result.append(multiplier(5))
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_person(self):
+        # What is wrong with this class definition that causes the test to fail?
+
+        # MODIFY IN HERE ONLY
+        
+        class Person(object):
+            def __init__(self, name, children=[]):
+                self.name = name
+                self.children = children
+
+            def add_child(self, child):
+                self.children.append(child)
+
+            def __repr__(self):
+                return '<Person %r>' % self.name
+
+        # END MODIFY IN HERE ONLY
+
+        # Create a family tree where dick is the parent of harry, and
+        # harry is the parent of tom. There are 2 ways of adding
+        # children - either by passing a list of children to the
+        # constructor, or by calling the add_child method.
+        tom = Person('tom')
+        harry = Person('harry')
+        harry.add_child(tom)
+        dick = Person('dick', children=[harry])
+
+        self.assertEqual(dick.children, [harry])
+        self.assertEqual(harry.children, [tom])
+        self.assertEqual(tom.children, [])
+
+    def test_forwarding(self):
+        # Define forwarding_property so that it acts as a descriptor
+        # such that getting it and setting it actually accesses a
+        # 'sub-attribute' of the parent object.
+
+        # MODIFY IN HERE ONLY
+
+        class forwarding_property(object):
+            def __init__(self, path):
+                pass
+
+        # END MODIFY IN HERE ONLY
+
+        # For example, instances of 'B' contain an instance of 'A'
+        # (called obj). Access to b.blah should be forwarded to
+        # b.obj.some_attribute.
+        class A(object):
+            some_attribute = 'old_value'
+            def __init__(self):
+                self.c_inst = C()
+
+        
+        class B(object):
+            # The 'blah' attribute should mirror 'obj.some_attribute'
+            blah = forwarding_property('obj.some_attribute')
+            c_size = forwarding_property('obj.c_inst.size')
+            def __init__(self):
+                self.obj = A()
+        
+        
+        class C(object):
+            def __init__(self):
+                self.size = 3
+        
+        
+        # Here's the test:
+        b = B()
+        self.assertEqual(b.blah, 'old_value')
+        b.blah = 'new_value'
+        b.c_size = 42
+        self.assertEqual(b.blah, 'new_value')
+        self.assertEqual(b.obj.some_attribute, 'new_value')
+        self.assertEqual(b.obj.c_inst.size, 42)
+
+
+if __name__ == '__main__':
+    unittest.main()
