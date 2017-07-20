f403a28fb70d1cc0d52165c8a60d4b558eca0ea5
ama002 <aashish.mahaswary@cambiumnetworks.com>
2017-07-20 11:46:20 +0530

    tests

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# tests1
-Onsite Python Tests
+This repository contains the following tests.
+
+# 1-csv_convert
+-------------
+
+Read the file "csv_question.txt" for more information. Create a script to
+convert a file to a CSV file. Example input files are included in the folder.
+
+# 2-unit_convert
+--------------
+
+The file "unit_convert.py" contains details of the test. All of the doc tests
+for the existing function fail. Write the code, using the doc string as a 
+guide, so that the tests pass.
---
+++ unit_convert/unit_convert.py
@@ -0,0 +1,43 @@
+# You should be able to use the python interpreter to run this script.
+# and it should display 5 test failures. You need to write the code
+# for the function so that all of the tests pass.
+#
+# IMPORTANT:
+# Only modify the code where indicated. Do not modify the intent of the test
+#
+
+
+def to_mhz_per_km(band_ghz, r_m, precision=2):
+    """Return the result in a fictitious unit of MHz/km.
+
+    band_ghz  : A band string in GHz.
+    r_m       : A string representing the range of the link in m.
+    precision : <optional> The display precision of the result. 
+    
+    This function should extract the numeric values, convert the values
+    to the appropriate units and then return a string showing the 
+    calculated result to the required precision. Don't forget the units.
+    
+    >>> to_mhz_per_km("3.2 GHz", "3248 m")
+    '985.22 MHz/km'
+    >>> to_mhz_per_km("4.5 GHz", "124.6 m", 3)
+    '36115.570 MHz/km'
+    >>> to_mhz_per_km("4.5 GHz", "124.6 m", 0)
+    '36116 MHz/km'
+    >>> to_mhz_per_km("6.2gHz", "4832.654M", 5)
+    '1282.93894 MHz/km'
+    >>> to_mhz_per_km("6gHz", "4832M")
+    '1241.72 MHz/km'
+    
+    """
+    # MODIFY IN HERE ONLY
+    
+    
+    # END MODIFY IN HERE ONLY
+ 
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
+
